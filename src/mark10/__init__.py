167c4d83570a7207575d1242c4b298dec3b9124e
dli <gdbbzq@gmail.com>
2023-07-26 20:20:09 -0700

    remove old files

--- src/mark10/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 30 11:43:56 2020
-
-@author: idealab
-"""
-
--- src/yuhao_efri/scripts/rosbag_record.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rosbag record /mocap_node/Robot_1/pose /mocap_node/Robot_2/pose /ni_daq_data
